5471e691251799e6a82ea150765976194946988e
lthihuongn <lilou33127v@gmail.com>
2023-09-07 12:23:30 +0200

    commit

--- lilou.c
+++ lilou.c
@@ -1,3 +1,19 @@
 //
 // Created by agath on 07/09/2023.
 //
+
+char work(char activity){
+    char code_fiscal;
+    if (activity == "bucheron"){
+        code_fiscal = 'B';
+    }else if (activity == "comptable"){
+        code_fiscal = 'C';
+    }else{
+        code_fiscal =  'A';
+    }
+    return code_fiscal;
+}
+
+int revenu_mensuel(int revenu_annuel){
+    return revenu_annuel/12;
+}
